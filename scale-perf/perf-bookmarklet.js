6e41dfccf3bdeae5978e9b69f0a0ca050b1e1d16
Thomas Heigl <thomas.heigl@iteratec.de>
2015-04-13 13:42:07 +0200

    New release

--- scale-perf/perf-bookmarklet.js
+++ scale-perf/perf-bookmarklet.js
@@ -489,7 +489,7 @@ ScalePerformanceBarClass.prototype = {
 		
 		isDev: function() {
 			// Flag if this script is executes locally or not
-			return (typeof(isDevelopmentMode) == "undefined" ? false : isDevelopmentMode;
+			return (typeof(isDevelopmentMode) == "undefined" ? false : isDevelopmentMode);
 		},
 		
 		/*
